932dd43ea56dc57c70679d7449b4dc78a69c0f57
Faniry Aomin <fanirintsoaharijaona@gmail.com>
2022-06-13 01:34:12 +0300

    dernière modification de l'authentification avec Django

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,10 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]
 
+STATIC_ROOT =  str(BASE_DIR.joinpath('staticfiles'))
+
+STATIC_FILES_STORAGE   =    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
